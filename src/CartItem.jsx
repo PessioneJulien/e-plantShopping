d783051abb792db60e84ea41f47152bccc52efdb
Julien Pessione <julien.pessione83@gmail.com>
2024-09-22 01:09:13 +0200

    Refactor CSS styles and fix bugs in CartSlice and CartItem components

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,35 +1,55 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { removeItem, updateQuantity, addItem } from './CartSlice';
 import './CartItem.css';
 
-const CartItem = ({ onContinueShopping }) => {
+const CartItem = ({ onContinueShopping, handleDeleteToCart }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount = 0;
+    cart.forEach(item => {
+      // item.cost is a string with a $ sign so we need to delete the $ sign and convert it to a number
+      const cost = item.cost.replace('$', '');
+      totalAmount += cost * item.quantity;
+    });
+    return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault();
+    onContinueShopping();
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(addItem({ name: item.name, image: item.image, cost: item.cost }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item.name));
+      handleDeleteToCart(item);
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
+    handleDeleteToCart(item);
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    // cost is $x so we need to delete the $ sign and convert it to a number
+    const cost = item.cost.replace('$', '');
+    return cost * item.quantity
   };
 
   return (
